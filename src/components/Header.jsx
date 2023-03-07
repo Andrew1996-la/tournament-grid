f9dbc6507d4d94c3302473db8b7288e881e1c1d6
Andrew1996-la <asoborev@mail.ru>
2023-03-08 01:52:08 +0300

    Стилизовал Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 function Header(props) {
   return (
-    <header>
-      this is header
-      <div></div>
+    <header className="h-[80px] bg-gray-300">
+      <h1 className="h-[100%] uppercase flex justify-center items-center">
+        Приложение для построения турнирных таблиц
+      </h1>
     </header>
   );
 }
